dcfa927e41b4d0e5186bcfa96ad09ce39dec5807
kirillRomanenko <romanenko.kirill1995@gmail.com>
2018-02-03 14:27:43 +0300

    редактирование карточки (Final)

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -15,14 +15,13 @@ export class ListComponent {
 
   }
 
-
   addlist(listName) {
     this.lists.push({
       idList: _.uniqueId('list_'),
       listName: listName
     });
-    console.log('listName:', listName);
   }
+
   removeList(list1) {
     _.pull(this.lists, list1);
   }
@@ -30,22 +29,18 @@ export class ListComponent {
   createCard(list) {
     this.cards.push({
       id: _.uniqueId('list_'),
-      description: list.Description,
+      description: list.description,
       list_id: list.idList
     });
   }
-  isEditing = false;
-  // EditingCard = null;
-  card = null;
+
+
   editCard(card) {
-  this.isEditing = true;
-  // this.EditingCard = _.cloneDeep(cards);
+    card.editing = true;
   }
-  updateCard(updatingCard) {
-   this.card = _.find(this.cards, {id: updatingCard.id});
-   this.card.description = updatingCard.description;
-   this.isEditing = false;
-   // this.EditingCard = null;
+
+  updateCard(card) {
+    card.editing = false;
   }
 
   deleteCard(cards) {
